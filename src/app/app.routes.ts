311be3fc5227d0fe8014a46d31d5e7d2d0a872fd
samndambuki <sam.ndambuki08@gmail.com>
2025-07-21 16:08:33 +0300

    worked on login and create account pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { CreateAccount } from './pages/create-account/create-account';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'login',pathMatch:'full'},
+    {path:'login',component:Login},
+    {path:'create-account',component:CreateAccount}
+];
---
+++ src/app/pages/create-account/create-account.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-create-account',
+  imports: [RouterModule],
+  templateUrl: './create-account.html',
+  styleUrl: './create-account.scss'
+})
+export class CreateAccount {
+
+}
