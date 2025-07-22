3d50c73899332e6b99047b26267155ed706c9c8d
samndambuki <sam.ndambuki08@gmail.com>
2025-07-22 14:55:14 +0300

    worked on form bindings and display error messages

--- src/app/pages/create-account/create-account.ts
+++ src/app/pages/create-account/create-account.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-create-account',
-  imports: [RouterModule],
+  imports: [RouterModule,ReactiveFormsModule,CommonModule],
   templateUrl: './create-account.html',
   styleUrl: './create-account.scss'
 })
 export class CreateAccount {
-
+createAccountForm = new FormGroup({
+email:new FormControl('',[Validators.required,Validators.email]),
+username:new FormControl('',[Validators.required,Validators.maxLength(12)]),
+password:new FormControl('',[Validators.required,Validators.minLength(6)]),
+})
 }
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule],
+  imports: [RouterModule,FormsModule,ReactiveFormsModule,CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
-
+  loginForm = new FormGroup({
+    email:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required,Validators.minLength(6)])
+  })
 }
