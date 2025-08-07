64f9698212ee965bef1a07d073578e3bd7c3f6c7
samndambuki <sam.ndambuki08@gmail.com>
2025-08-07 11:23:31 +0300

    configured prettier code formatter

--- src/app/services/user-service.ts
+++ src/app/services/user-service.ts
@@ -4,23 +4,23 @@ import { Observable } from 'rxjs';
 import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   private http = inject(HttpClient);
   private baseUrl = 'http://localhost:3000';
 
-  user = signal<User|null>(null);  
+  user = signal<User | null>(null);
 
-  createAccount(userObj:User):Observable<User>{
-    return this.http.post<User>(`${this.baseUrl}/users`,userObj)
+  createAccount(userObj: User): Observable<User> {
+    return this.http.post<User>(`${this.baseUrl}/users`, userObj);
   }
-  getUser(email:string):Observable<User[]>{
-    const params = new HttpParams().set('email',email);
-    return this.http.get<User[]>(`${this.baseUrl}/users`,{params})
+  getUser(email: string): Observable<User[]> {
+    const params = new HttpParams().set('email', email);
+    return this.http.get<User[]>(`${this.baseUrl}/users`, { params });
   }
 
-  setUser(user:User|null){
+  setUser(user: User | null) {
     this.user.set(user);
   }
 }
